4f55a57670474b00f0b36aa450cd799d20922af8
EdgarAraya <44482655+EdgarAraya@users.noreply.github.com>
2020-10-30 15:23:56 -0300

    laptop, test

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService} from '../message.service';
-
+//test
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
